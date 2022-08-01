20c02ea794b993f5ea7120088c7fc765dc59d6ad
juank <juank@DESKTOP-IVL3QEL>
2022-08-01 14:57:01 -0500

    hola mundo

---
+++ ProyectoLibreria/src/paquete/Ejemplo.java
@@ -0,0 +1,12 @@
+package paquete;
+
+import javax.swing.JOptionPane;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "hola mundo git");
+	}
+
+}
